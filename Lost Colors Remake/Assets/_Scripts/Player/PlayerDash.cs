1a1ee6e6ad9cc63612ffa16c17926c5e220c26bc
Emrys <legall.emrys@gmail.com>
2025-05-14 18:29:27 +0200

    un peu venère la force du dash

--- Lost Colors Remake/Assets/_Scripts/Player/PlayerDash.cs
+++ Lost Colors Remake/Assets/_Scripts/Player/PlayerDash.cs
@@ -5,7 +5,7 @@ public class PlayerDash : MonoBehaviour
     public void DashOnClick()
     {
         SimpleDash dash = (SimpleDash)SpellManager.Instance.GetSpell("SimpleDash");
-        SkillContext context = new(PlayerMain.Instance.Rigidbody2D, PlayerMain.Instance.gameObject, PlayerMain.Instance.Move._moveInput, 35);
+        SkillContext context = new(PlayerMain.Instance.Rigidbody2D, PlayerMain.Instance.gameObject, PlayerMain.Instance.Move._moveInput, 25);
         dash.Activate(context);
     }
 }

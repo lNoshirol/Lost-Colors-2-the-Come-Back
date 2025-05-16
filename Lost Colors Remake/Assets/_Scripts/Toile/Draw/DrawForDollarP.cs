b8991f3c8e3f5ace8f47aeb370c4bda05fffd0fa
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-05-16 10:26:51 +0200

    Tout de suuite, la météo : IL FAIT TARPIN CHAUUUD

--- Lost Colors Remake/Assets/_Scripts/Toile/Draw/DrawForDollarP.cs
+++ Lost Colors Remake/Assets/_Scripts/Toile/Draw/DrawForDollarP.cs
@@ -110,7 +110,7 @@ public class DrawForDollarP : MonoBehaviour
             //TryMakeAdaptativeCollider(GetDrawCenter(points), gestureResult);
 
             //_drawData = new DrawData(points, GetDrawDim(points), gestureResult, GetSpellTargetPointFromCenter(points), ColorUtility.ToHtmlStringRGB(_currentColor));
-            if (gestureResult.Score < 0.8 && _detectEnemyInShape.)
+            if (gestureResult.Score < 0.8)
             {
                 touchingScreen = false;
 

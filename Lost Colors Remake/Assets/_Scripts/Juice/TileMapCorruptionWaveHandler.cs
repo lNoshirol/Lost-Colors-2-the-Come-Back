cdb2038fdfdf80d46bf46be987d82a18beefcac4
JElouann <elouann.jouenne@e-artsup.net>
2025-05-17 20:02:31 +0200

    patch color wave handler

--- Lost Colors Remake/Assets/_Scripts/Juice/TileMapCorruptionWaveHandler.cs
+++ Lost Colors Remake/Assets/_Scripts/Juice/TileMapCorruptionWaveHandler.cs
@@ -4,7 +4,10 @@ using UnityEngine.Tilemaps;
 
 public class TileMapCorruptionWaveHandler : MonoBehaviour
 {
-    [SerializeField] private Vector2 _waveStartPoint;
+    [SerializeField] public Vector2 _waveStartPoint, _waveRatio;
+    public float _waveWidth, WaveProgress;
+
+
     private TilemapRenderer _tilemapRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,12 +26,25 @@ public class TileMapCorruptionWaveHandler : MonoBehaviour
         {
             WaveAnim();
         }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        MaterialPropertyBlock propertyBlock = new();
+        propertyBlock.SetVector("_WaveDiffusionStartPoint", _waveStartPoint);
+        propertyBlock.SetFloat("_WaveProgress", 0f);
+        _tilemapRenderer.SetPropertyBlock(propertyBlock);
     }
 
     private void WaveAnim()
     {
         MaterialPropertyBlock mPropertyBlock = new();
         _tilemapRenderer.material.DOFloat(0f, "_WaveProgress", 0f);
+        WaveProgress = _tilemapRenderer.material.GetFloat("_WaveProgress");
         mPropertyBlock.SetVector("_WaveDiffusionStartPoint", _waveStartPoint);
         _tilemapRenderer.SetPropertyBlock(mPropertyBlock);
         _tilemapRenderer.material.DOFloat(20f, "_WaveProgress", 2.4f);

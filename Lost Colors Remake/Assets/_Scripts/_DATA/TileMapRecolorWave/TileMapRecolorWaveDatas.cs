3a5d047e0673758b17365d83a5fe885f4dda115a
JElouann <elouann.jouenne@e-artsup.net>
2025-05-23 10:40:38 +0200

    J'ai rangé et nettoyé ma chambre

---
+++ Lost Colors Remake/Assets/_Scripts/_DATA/TileMapRecolorWave/TileMapRecolorWaveDatas.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileMapRecolorWaveDatas", menuName = "Scriptable Objects/TileMapRecolorWaveDatas")]
+public class TileMapRecolorWaveDatas : ScriptableObject
+{
+    
+}

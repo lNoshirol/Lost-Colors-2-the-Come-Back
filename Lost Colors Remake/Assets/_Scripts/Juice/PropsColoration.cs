daeb86ec8edbed8a3faf017377d0ce80b2e260ca
JElouann <elouann.jouenne@e-artsup.net>
2025-06-02 10:31:37 +0200

    jsp

---
+++ Lost Colors Remake/Assets/_Scripts/Juice/PropsColoration.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class PropsColoration : MonoBehaviour
+{
+    private float _timeBeforeTouch;
+
+    void Start()
+    {
+        float distance = Vector3.Distance(CrystalManager.Instance._ListCrystal[0].CrystalName.transform.position, this.transform.position);        
+        
+    }
+
+    private async void OnCrystalColorize()
+    {
+
+    }
+}

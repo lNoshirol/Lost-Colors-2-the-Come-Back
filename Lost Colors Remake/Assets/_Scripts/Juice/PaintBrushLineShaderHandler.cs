fe2d937b1a340ad33542a44074f963e2f143d6ee
JElouann <elouann.jouenne@e-artsup.net>
2025-06-05 19:37:44 +0200

    j'essaie, j'essaie, de faire de mon mieuuux

--- Lost Colors Remake/Assets/_Scripts/Juice/PaintBrushLineShaderHandler.cs
+++ Lost Colors Remake/Assets/_Scripts/Juice/PaintBrushLineShaderHandler.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float _lineLength;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            _lineLength = ExternalDrawFunctions.GetDrawLength(DrawForDollarP.instance.GetDrawPoints());
+
+        }
     }
 }

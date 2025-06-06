9367fa3b29806d62405abeeeffc82daa1c05b5fa
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-06-06 17:04:13 +0200

    pk j'ai des changes ici fdp ?

---
+++ Lost Colors Remake/Assets/_Scripts/Toile/PasDinspiPourLeNomDesoLaTeam.cs
@@ -0,0 +1,100 @@
+using PDollarGestureRecognizer;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasDinspiPourLeNomDesoLaTeam : MonoBehaviour
+{
+    [SerializeField] private GameObject closestEnemyDetected;
+
+    private void Start()
+    {
+        DrawForDollarP.instance.OnDrawFinish += DrawFinish;
+    }
+
+
+    public void DrawFinish(DrawData result)
+    {
+        if (EnemyManager.Instance.DoEnemyOnScreenHaveRecognizedGlyph(result.result.GestureClass))
+        {
+            StartCoroutine(GoodDraw(result));
+            closestEnemyDetected = EnemyManager.Instance.FindClosestEnemyWithGlyph(result.result.GestureClass);
+        }
+        else if (EnemyManager.Instance.FindAllEnnemyOnScreen().Count > 0) 
+        {
+            StartCoroutine(WrongDraw(result));
+        }
+    }
+
+    public IEnumerator GoodDraw(DrawData result)
+    {
+        Time.timeScale = 0;
+        Debug.LogWarning($"Play sound DrawSuccess {Time.timeScale}");
+        yield return new WaitForSecondsRealtime(0.1f);
+        closestEnemyDetected.TryGetComponent(out EnemyMain enemyMain);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(false);
+        DrawForDollarP.instance.SetActiveLine(false);
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(true);
+        DrawForDollarP.instance.SetActiveLine(true);
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(false);
+        DrawForDollarP.instance.SetActiveLine(false);
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(true);
+        DrawForDollarP.instance.SetActiveLine(true);
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(false);
+        DrawForDollarP.instance.SetActiveLine(false);
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        enemyMain.Armor.activeGlyphs[0].SetActive(true);
+        DrawForDollarP.instance.SetActiveLine(true);
+        Time.timeScale = PlayerMain.Instance.toileInfo.slowMotionScale;
+    }
+
+    public IEnumerator WrongDraw(DrawData result)
+    {
+        Debug.LogWarning("Play sound DrawBad");
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(1);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    #region Debug et Test
+
+    public Slider slider;
+    public float time;
+    public float unscaledTime;
+
+
+    private void Update()
+    {
+        time = Time.time;
+        unscaledTime = Time.unscaledTime;
+    }
+
+    public void ChangeScale()
+    {
+        Time.timeScale = slider.value;
+    }
+
+    #endregion
+}

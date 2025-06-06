49a10d26e1521389046c32e64b4188955ed0ddfc
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-06-06 19:47:10 +0200

    prb crystal patch

--- Lost Colors Remake/Assets/_Scripts/Enigme/Draw/Enigma.cs
+++ Lost Colors Remake/Assets/_Scripts/Enigme/Draw/Enigma.cs
@@ -16,10 +16,10 @@ public class Enigma : MonoBehaviour
 
     private void SolveEnigme(DrawData drawData)
     {
-        if (Vector3.Distance(PlayerMain.Instance.transform.position, transform.position) <= castDistanceAuthorize && drawData.result.GestureClass == drawTargetName && !_isSolved)
+        if (Vector3.Distance(PlayerMain.Instance.transform.position, transform.position) <= castDistanceAuthorize && drawData.result.GestureClass == drawTargetName && drawData.result.Score > 0.9f && !_isSolved)
         {
             OnEnigmaSolve?.Invoke();
-            //Debug.Log("Choix numéro 2 ça marche");
+            Debug.Log("Choix numéro 2 ça marche");
         }
     }
 

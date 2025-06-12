58b51796d44b1d0c5a8811c6205ed6802730ab06
JElouann <elouann.jouenne@e-artsup.net>
2025-06-12 23:48:06 +0200

    bo dash

--- Lost Colors Remake/Assets/_Scripts/Juice/PlayerDashVFX.cs
+++ Lost Colors Remake/Assets/_Scripts/Juice/PlayerDashVFX.cs
@@ -1,28 +1,42 @@
 using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerDashVFX : MonoBehaviour
 {
     [SerializeField] private GameObject _spriteTrailPrefab;
     [SerializeField] private int _numOfIntermediateSprites;
+    [SerializeField] private TrailRenderer _trail;
     private Pool _spritesPool;
 
     void Start()
     {
+        _trail.emitting = false;
         _spritesPool = new(_spriteTrailPrefab, _numOfIntermediateSprites, this.transform);
     }
 
     public IEnumerator BalanceSpriteTrail()
     {
+        _trail.emitting = true;
+        List<GameObject> usedSprites = new();
         for (int i = 0; i < _numOfIntermediateSprites; i++)
         {
             GameObject sprite = _spritesPool.GetObject();
-            sprite.transform.SetParent(null, false);
-            sprite.transform.position = PlayerMain.Instance.transform.position;
+            sprite.transform.SetParent(null);
+            sprite.transform.position = PlayerMain.Instance.transform.position + Vector3.up * 0.4f;
             sprite.TryGetComponent(out SpriteRenderer renderer);
             renderer.flipX = PlayerMain.Instance.Move._moveInput.x < 0;
+            renderer.DOFade(0, 0.2f);
+            usedSprites.Add(sprite);
             yield return new WaitForSeconds(0.1f);
         }
-        
+        _trail.emitting = false;
+        foreach (GameObject sprite in usedSprites)
+        {
+            _spritesPool.Stock(sprite);
+            sprite.TryGetComponent(out SpriteRenderer renderer);
+            renderer.DOFade(1, 0f);
+        }
     }
 }

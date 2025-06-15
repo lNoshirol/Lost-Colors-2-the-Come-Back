8a64569473aaf90f65d6144cb241b66608db9de1
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2025-06-15 13:57:04 +0200

    mb

--- Lost Colors Remake/Assets/_Scripts/Sound/SoundHasToDesactivate.cs
+++ Lost Colors Remake/Assets/_Scripts/Sound/SoundHasToDesactivate.cs
@@ -5,12 +5,7 @@ public class SoundHasToDesactivate : MonoBehaviour
 {
     [SerializeField] private AudioSource m_AudioSource;
 
-    private void Start()
-    {
-        StartCoroutine(WaitToDesactivate());
-    }
-
-    private IEnumerator WaitToDesactivate()
+    public IEnumerator WaitToDesactivate()
     {
         yield return new WaitForSeconds(m_AudioSource.clip.length);
         SoundsManager.Instance._poolObjectSound.GetPoolSound().Stock(gameObject);

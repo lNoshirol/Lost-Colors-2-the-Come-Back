632dd8e7324a1ce36c3073f68383555fa03a106e
JElouann <elouann.jouenne@e-artsup.net>
2025-06-15 14:06:12 +0200

    PP and crystal adjustements

--- Lost Colors Remake/Assets/_Scripts/Enigme/EnigmeSolvedCrystal.cs
+++ Lost Colors Remake/Assets/_Scripts/Enigme/EnigmeSolvedCrystal.cs
@@ -11,6 +11,11 @@ public class EnigmeSolvedCrystal : EnigmeSolved
     public Sprite crystalUncorrupted;
     [SerializeField] private Volume _localDarkVolume;
 
+    [Header("Global Volume")]
+    [SerializeField] private Volume _globalVolume;
+    [SerializeField] private VolumeProfile _BWVolumeProfile;
+    [SerializeField] private VolumeProfile _ColoredVolumeProfile;
+
     public override void Interact()
     {
         var Sound = SoundsManager.Instance;
@@ -22,7 +27,8 @@ public class EnigmeSolvedCrystal : EnigmeSolved
 
     private void CrystalColorLerp()
     {
-        StartCoroutine(FadePPVolume());
+        StartCoroutine(FadeLocalVolume());
+        StartCoroutine(SwitchGlobalVolume());
         crystal.material.SetTexture("_ColoredTex", crystalUncorrupted.texture);
         crystal.material.DOFloat(1f, "_Transition", 2f);
         var Sound = SoundsManager.Instance;
@@ -30,11 +36,26 @@ public class EnigmeSolvedCrystal : EnigmeSolved
         Sound.PlaySound(Sound.Activate[RandomSound], false);
     }
 
-    private IEnumerator FadePPVolume()
+    private IEnumerator FadeLocalVolume()
     {
         for (float i = 1; i > 0; i-=0.01f) 
         {
             _localDarkVolume.weight = i;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private IEnumerator SwitchGlobalVolume()
+    {
+        for (float i = 1; i > 0; i-=0.01f)
+        {
+            _globalVolume.weight = i;
+            yield return new WaitForSeconds(0.01f);
+        }
+        _globalVolume.profile = _ColoredVolumeProfile;
+        for (float j = 0; j < 1; j += 0.01f)
+        {
+            _globalVolume.weight = j;
             yield return new WaitForSeconds(0.01f);
         }
     }

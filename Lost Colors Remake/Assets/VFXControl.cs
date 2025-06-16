68fbe7beda7d08ea6edefa48c1f09b6458bd5fdb
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2025-06-16 23:41:33 +0200

    VFX_Flame

--- Lost Colors Remake/Assets/VFXControl.cs
+++ Lost Colors Remake/Assets/VFXControl.cs
@@ -13,29 +13,28 @@ public class VFXControl : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         animator.SetBool(transition1Name, true);
         yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0).Length);
-        EnemyManager.Instance.RePackInPool(gameObject, EnemyManager.Instance.vfxPool);
+        if (gameObject.name.StartsWith("VFX_Thunder"))
+        {
+            EnemyManager.Instance.RePackInPool(gameObject, EnemyManager.Instance.vfxPool);
+        }
     }
 
     public void OnPlay()
     {
-        Debug.LogWarning("YO JE SUIS LE LOUP");
         PlaySound();
         StartCoroutine(WaitForNext(transition1WaitTime));
     }
 
     public void PlaySound()
     {
-        Debug.Log("JE JOUE UN SON");
         if (gameObject.name.StartsWith("VFX_Thunder"))
         {
-            Debug.Log("VFX_Thunder");
             var sound = SoundsManager.Instance;
             int RandowDeer = Random.Range(0, sound.SoundEffectDeerAttackRanged.Length);
             sound.PlaySound(sound.SoundEffectDeerAttackRanged[RandowDeer], false);
         }
         else if (gameObject.name.StartsWith("VFX_FlameThrower"))
         {
-            Debug.Log("VFX_FlameThrower");
             var sound = SoundsManager.Instance;
             int RandowDeer = Random.Range(0, sound.SoundEffectWolfAttackRanged.Length);
             sound.PlaySound(sound.SoundEffectWolfAttackRanged[RandowDeer], false);
